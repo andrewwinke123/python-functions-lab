ba7b311ba034cb7ccce5112d34535ff9580dc20a
andrewwinke123 <andrewwinke@gmail.com>
2023-05-23 23:10:07 -0400

    solve 1

---
+++ python_functions.py
@@ -0,0 +1,53 @@
+#problem 1
+
+def sum_to(n):
+
+    return (n * (n + 1) // 2)
+
+print(sum_to(6))
+print(sum_to(10))
+
+
+
+
+
+
+
+
+#problem 2
+
+def largest():
+
+    return()
+
+
+
+
+
+
+#problem 3
+
+
+
+def occurrences():
+
+    return()
+
+
+
+
+
+
+
+
+
+#problem  4
+
+
+def product():
+
+    return()
+
+
+
+
