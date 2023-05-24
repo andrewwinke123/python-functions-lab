99cc891845222c04bdd6a914cdc86004f615df9a
andrewwinke123 <andrewwinke@gmail.com>
2023-05-24 16:10:45 -0400

    solve 3 and 4

--- python_functions.py
+++ python_functions.py
@@ -14,12 +14,12 @@ print(sum_to(10))
 
 #problem 2
 
-def largest():
-
-    return()
-
+def largest(nums):
+    return max(nums)
 
 
+print(largest([1, 2, 3, 4, 0]))
+print(largest([10, 4, 2, 321, 91, 54]))
 
 
 
@@ -27,13 +27,14 @@ def largest():
 
 
 
-def occurrences():
-
-    return()
-
+def occurrences(string1, string2):
+    return string1.count(string2)
 
 
-
+print(occurrences('fleep floop', 'e')) 
+print(occurrences('fleep floop', 'p'))
+print(occurrences('fleep floop', 'ee'))
+print(occurrences('fleep floop', 'fe'))
 
 
 
